936352b30f022e5ead50e992c03d122530a531b2
Jackson Zhang <zbcjackson@gmail.com>
2014-03-19 14:45:27 +0800

    Create project structure

---
+++ src/com/odde/tennis/Referee.java
@@ -0,0 +1,15 @@
+package com.odde.tennis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Referee extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
