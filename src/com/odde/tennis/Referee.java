6d72fa39bdabcb918136273a4a0c11493f43565e
Jackson Zhang <zbcjackson@gmail.com>
2014-03-19 16:48:10 +0800

    Fifteen Love after Player1 scores

--- src/com/odde/tennis/Referee.java
+++ src/com/odde/tennis/Referee.java
@@ -2,6 +2,9 @@ package com.odde.tennis;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Referee extends Activity {
     /**
@@ -11,5 +14,11 @@ public class Referee extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+
+    public void player1Score(View view) {
+        TextView scoreView = (TextView) findViewById(R.id.scoreView);
+        scoreView.setText("Fifteen Love");
     }
 }
--- test/RefereeActivityTest.java
+++ test/RefereeActivityTest.java
@@ -1,3 +1,4 @@
+import android.widget.Button;
 import android.widget.TextView;
 import com.odde.tennis.R;
 import com.odde.tennis.Referee;
@@ -11,9 +12,20 @@ import static org.junit.Assert.assertEquals;
 @RunWith(RobolectricTestRunner.class)
 public class RefereeActivityTest {
     @Test
-    public void display_score(){
+    public void display_Love_All_as_init_score(){
         Referee referee = Robolectric.buildActivity(Referee.class).create().get();
         TextView scoreView = (TextView) referee.findViewById(R.id.scoreView);
         assertEquals("Love All", scoreView.getText().toString());
+    }
+
+    @Test
+    public void fifteen_love_after_player1_scores(){
+        Referee referee = Robolectric.buildActivity(Referee.class).create().get();
+        Button player1Score = (Button) referee.findViewById(R.id.player1Score);
+        TextView scoreView = (TextView) referee.findViewById(R.id.scoreView);
+
+        player1Score.performClick();
+
+        assertEquals("Fifteen Love", scoreView.getText().toString());
     }
 }
